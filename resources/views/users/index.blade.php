ad63bc7bf48bdbb071aac64d46caee3bdfac61d1
Taha Kaan Yılmaz <kaanylmz34@icloud.com>
2024-08-14 20:28:15 +0300

    Admin rolü için "Kullanıcılar" menüsü tasarımı ve CRUD operasyonları - Rötuş

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -13,6 +13,16 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
+                    @if ($errors->any())
+                        <div class="mb-4">
+                            <div class="font-medium text-red-600">Hata!</div>
+                            <ul class="mt-3 list-disc list-inside text-sm text-red-600">
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
                     <table class="min-w-full divide-y divide-gray-200">
                         <thead>
                             <tr>
@@ -36,8 +46,18 @@
                                             </span>
                                         @endforeach
                                     </td>
-                                    <td class="px-6 py-4 whitespace-no-wrap text-right text-sm leading-5 font-medium">
+                                    <td class="px-6 py-4 whitespace-no-wrap text-right text-sm leading-5 font-medium flex gap-x-6 justify-end">
                                         <a href="{{ route('user.show', $user) }}" class="text-indigo-600 hover:text-indigo-900">Görüntüle</a>
+                                        @can('user.edit')
+                                            <a href="{{ route('user.edit', $user) }}" class="text-indigo-600 hover:text-indigo-900">Düzenle</a>
+                                        @endcan
+                                        @can('user.destroy')
+                                            <form method="POST" action="{{ route('user.destroy', $user) }}" onsubmit="return confirm('Silmek istediğinize emin misiniz?')">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="text-red-600 hover:text-red-900">Sil</button>
+                                            </form>
+                                        @endcan
                                     </td>
                                 </tr>
                             @endforeach
