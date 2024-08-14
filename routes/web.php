2f9917ae6a2fe77046488dfe501a096873a6db44
Taha Kaan Yılmaz <kaanylmz34@icloud.com>
2024-08-14 18:27:16 +0300

    Genel Kurulum ve Laravel Konfigürasyonu

--- routes/web.php
+++ routes/web.php
@@ -3,18 +3,26 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
+
+    # Yönlendirme Sayfası
+    Route::get('/', function () {
+        return redirect()->route('dashboard');
+    });
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+});
+
+Route::middleware(['auth', 'verified'])->group(function () {
+
+    # Anasayfa
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
 });
 
 require __DIR__.'/auth.php';
